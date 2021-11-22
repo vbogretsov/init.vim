9c4446307107961253c56f91683743e6c35b9e8a
Vladimir Bogretsov <v.bogretsov@itransition.com>
2021-11-22 21:19:02 +0300

    Fixed luasnip integration

--- lua/plugins/nvimcmp.lua
+++ lua/plugins/nvimcmp.lua
@@ -23,7 +23,25 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+    -- ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+    -- ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+          cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+      else
+          fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+          cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+      else
+          fallback()
+      end
+    end,
   },
 }
